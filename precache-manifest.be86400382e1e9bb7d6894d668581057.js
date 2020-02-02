6ce16b2c9a3dcebef1e0d492da920beca1bfbdf9
Vasile Claudiu <claudiu.vasile@gmail.com>
2020-02-02 13:10:34 +0200

    Second Commit

---
+++ precache-manifest.be86400382e1e9bb7d6894d668581057.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e72177e148ee480664643380132751a1",
+    "url": "/reactnoteandtodo/index.html"
+  },
+  {
+    "revision": "ac9f90237b8004bc04dc",
+    "url": "/reactnoteandtodo/static/css/2.323bf276.chunk.css"
+  },
+  {
+    "revision": "2bf16ed29ec8dc5549d5",
+    "url": "/reactnoteandtodo/static/css/main.0a4fc402.chunk.css"
+  },
+  {
+    "revision": "ac9f90237b8004bc04dc",
+    "url": "/reactnoteandtodo/static/js/2.94161e2d.chunk.js"
+  },
+  {
+    "revision": "2bf16ed29ec8dc5549d5",
+    "url": "/reactnoteandtodo/static/js/main.00654410.chunk.js"
+  },
+  {
+    "revision": "8a5a447417e4f3d574f8",
+    "url": "/reactnoteandtodo/static/js/runtime-main.7b5a5aed.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactnoteandtodo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactnoteandtodo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactnoteandtodo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactnoteandtodo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactnoteandtodo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactnoteandtodo/static/media/logo.25bf045c.svg"
+  }
+]);
